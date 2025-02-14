8436d1116488eefb31690b9c43e056d26dd0d728
Mike Storey <mike@agile-learning.institute>
2025-02-14 14:47:35 -0500

    playing with message, channel info

--- simple.py
+++ simple.py
@@ -22,15 +22,23 @@ async def on_message(message):
         print("Echo, echo, echo")
         return
     
-    if message.channel.name not in ["fran","workshop-be1df368-9169-442e-8f63-0bc4a5df7b54"]:
+    ctype = message.channel.type
+    print(f"Channel Type: {ctype} is {type(ctype)}")
+    if message.channel.type != discord.ChannelType.private and message.channel.name not in ["fran","workshop"]:
         print(f"Just snooping on {message.channel}")
         return
     
+    guild = message.guild
     user_name = message.author
     user_message = message.content
     channel = message.channel
-    category = channel.category
-    guild = message.guild
+
+    channel_id = channel.id
+    channel_name = "DM"
+    channel_category = "Direct"
+    if channel.type != discord.ChannelType.private:
+        channel_name = channel.name
+        channel_category = channel.category
     
     response = ollama.chat(model='llama3.2:latest', messages=[
         {
@@ -38,8 +46,8 @@ async def on_message(message):
             'content': user_message,
         },
     ])
-    print(f"Guild {guild}, Category {category}, Channel {channel}, user {user_name}, message {user_message}, ollama says {response}")
-    await message.channel.send(f"Hello {user_name} from {guild} on {channel} in {category} - Ollama says: {response.message.content}")
+    print(f"Guild {guild}, Category {channel_category}, Channel {channel_name}-{channel_id}, user {user_name}, message {user_message}, ollama says {response}")
+    await message.channel.send(f"Hello {user_name} from {guild} on {channel} in {channel_category} - Ollama says: {response.message.content}")
 
 # Run bot
 client.run(TOKEN)

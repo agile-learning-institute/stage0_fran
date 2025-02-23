d3faedcf0daa8021f369cac9ce218f32091aebbf
Mike Storey <mike@agile-learning.institute>
2025-02-23 17:41:18 -0500

    Config Agent setup and tested

--- src/agents/config_agent.py
+++ src/agents/config_agent.py
@@ -1,25 +1,36 @@
 import logging
-from echo import Blueprint
-from config.config import Config
 from echo_utils.breadcrumb import create_breadcrumb
 from echo_utils.token import create_token
+from config.config import Config
 
 logger = logging.getLogger(__name__)
 
 def create_config_agent(bot):
-    """ Registers event handlers and commands for the Fran home channel. """
-    config_agent = Blueprint('conversation_agent', __name__)
+    """ Registers event handlers and commands for the Config Agent. """
 
-    @config_agent.action(action_name="get_config", 
+    def get_config(arguments):
+        """ Slash command to get config data"""
+        try:
+            token = create_token()
+            breadcrumb = create_breadcrumb(token)
+            config = Config.get_instance()
+            logger.info(f"get_config Success {breadcrumb}")
+            return config.to_dict(token=token)
+        except Exception as e:
+            logger.warning(f"get_config Error has occurred: {e}")
+            return "error"
+    bot.register_action(
+        action_name="get_config", 
+        function=get_config,
         description="Get the Bot Configuration information", 
-        arguments_schema={},
-        outputs_schema={
+        arguments_schema="none",
+        output_schema={
             "type": "object",
             "description": "",
             "properties": {
                 "config_items": {
                     "description": "List of Configuration Items and non-secret values",
-                   "type": "array"
+                "type": "array"
                 },
                 "versions": {
                     "description": "List of database collection versions",
@@ -36,14 +47,5 @@ def create_config_agent(bot):
             }
         }
     )
-    async def get_config(arguments):
-        """ Slash command to get config data"""
-        try:
-            token = create_token()
-            breadcrumb = create_breadcrumb(token)
-            config = Config.get_instance()
-            logger.info(f"Get Exercise Success {breadcrumb}")
-            return config
-        except Exception as e:
-            logger.warning(f"Get Exercise Action Error has occurred: {e}")
-            return "A processing error occurred"
+
+    logger.info("Registered config agent action handlers.")
--- src/agents/config_agent_test.py
+++ src/agents/config_agent_test.py
@@ -1,12 +1,58 @@
 import unittest
+from unittest.mock import patch, MagicMock
+from echo.agent import Agent
+from agents.config_agent import create_config_agent
 
 class TestConfigAgent(unittest.TestCase):
-
+    
     def setUp(self):
-        """Re-initialize the config for each test."""
+        """Set up a mock bot agent."""
+        self.mock_bot = Agent("test_bot_agent")
+        self.mock_config = {}
+        create_config_agent(self.mock_bot)
+        self.get_config = self.mock_bot.actions["get_config"]["function"]
 
-    def test_something(self):
-        self.assertEqual(True, True)
-
-if __name__ == '__main__':
+    @patch("agents.config_agent.create_token")  
+    @patch("agents.config_agent.create_breadcrumb")  
+    @patch("agents.config_agent.Config.to_dict")
+    def test_get_configs_success(self, mock_config_to_dict, mock_create_breadcrumb, mock_create_token):
+        """Test successful execution of get_configs action."""
+       
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_config_to_dict.return_value = "config_value"
+        
+        # Call function
+        arguments = ""
+        result = self.get_config(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_config_to_dict.assert_called_once_with(token="fake_token")
+        self.assertEqual(result, "config_value")
+    
+    @patch("agents.config_agent.create_token")  
+    @patch("agents.config_agent.create_breadcrumb")  
+    @patch("agents.config_agent.Config.to_dict")
+    def test_get_configs_success(self, mock_config_to_dict, mock_create_breadcrumb, mock_create_token):
+        """Test successful execution of get_configs action."""
+       
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_config_to_dict.side_effect = Exception("Test Exception")
+        
+        # Call function
+        arguments = ""
+        result = self.get_config(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_config_to_dict.assert_called_once_with(token="fake_token")
+        self.assertEqual(result, "error")
+                    
+if __name__ == "__main__":
     unittest.main()

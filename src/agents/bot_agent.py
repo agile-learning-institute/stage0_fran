98f2ddd15c2237bf7a7e647bd2dc6ce61ca1b5c4
Mike Storey <mike@agile-learning.institute>
2025-02-23 15:07:59 -0500

    bot_agent filled out and tested

--- src/agents/bot_agent.py
+++ src/agents/bot_agent.py
@@ -1,24 +1,167 @@
-import discord
 import logging
-from discord import app_commands
+from echo_utils.breadcrumb import create_breadcrumb
+from echo_utils.token import create_token
 from services.bot_services import BotServices
-from src.services.workshop_services import WorkshopServices
 
 logger = logging.getLogger(__name__)
 
 def create_bot_agent(bot):
     """ Registers event handlers and commands for the Fran home channel. """
-    @bot.action(action_name="get_bot", 
-                description="", 
-                arguments_schema={},
-                outputs_schema={})
-    async def get_bot(arguments):
-        """  """
+    
+    def get_bot(arguments):
+        """Get the bot record based on the bot_id passed as arguments"""
         try:
-            await bot = BotServices.get_bot(arguments._id)
+            token = create_token()
+            bot = BotServices.get_bot(arguments, token)
             return bot
         except Exception as e:
-            logger.warning(f"Agent Get bot Error has occurred: {e}")
+            logger.warning(f"A get_bot Error has occurred: {e}")
             return "error"
-        
+    bot.register_action(
+        action_name = "get_bot", 
+        function = get_bot,
+        description = "Get all bot information for the specified bot", 
+        arguments_schema = {
+            "description": "Bot Unique Identifier",
+            "type": "identifier"
+        },
+        output_schema = {
+            "title": "Bots",
+            "description": "Stage0 Bots",
+            "type": "object",
+            "properties": {
+                "_id": {
+                    "description": "The unique identifier for a Bot Description",
+                    "type": "identifier"
+                },
+                "status": {
+                    "description": "The status of the Bot",
+                    "type": "enum",
+                    "enums": "default_status"
+                },
+                "name": {
+                    "description": "Bot Short Name",
+                    "type": "word"
+                },
+                "description": {
+                    "description": "Short description of bot",
+                    "type": "sentence"
+                },
+                "channels": {
+                    "description": "List of Discord Channel ID values",
+                    "type": "array",
+                    "items": {
+                        "type": "word"
+                    }
+                },
+                "last_saved": {
+                    "description": "Last Saved breadcrumb",
+                    "type": "breadcrumb"
+                }
+            }
+        })
+
+    def get_channels(arguments):
+        """Get a list of active channels for the specified bot"""
+        try:
+            token = create_token()
+            bot = BotServices.get_channels(arguments, token)
+            return bot
+        except Exception as e:
+            logger.warning(f"A get_channels Error has occurred: {e}")
+            return "error"
+    bot.register_action(
+        action_name= "get_channels", 
+        function = get_channels,
+        description = "Get a list of active channels", 
+        arguments_schema = {
+            "description": "Bot Unique Identifier",
+            "type": "identifier"
+        },
+        output_schema = {
+            "description": "Array (set) of channel ID's",
+            "type": "array",
+            "items": {
+                "type": "identifier"
+            }
+        })
+    
+    def add_channel(arguments):
+        """Add the specified channel_id 
+        to the set of active channels for bot_id"""
+        try:
+            token = create_token()
+            breadcrumb = create_breadcrumb(token)
+            bot = BotServices.add_channel(
+                arguments["bot_id"], arguments["channel_id"], 
+                token, breadcrumb)
+            return bot
+        except Exception as e:
+            logger.warning(f"A add_channel Error has occurred: {e}")
+            return "error"
+    bot.register_action(
+        action_name= "add_channel", 
+        function = add_channel,
+        description = "Add a channel to the set of active channels", 
+        arguments_schema = {
+            "description": "Bot and Channel identifier",
+            "type": "object",
+            "properties": {
+                "bot_id": {
+                    "description": "The bot unique identifier",
+                    "type": "identifier"
+                },
+                "channel_id": {
+                    "description": "The channel identifier",
+                    "type": "identifier"
+                }
+            }
+        },
+        output_schema = {
+            "description": "Array (set) of channel ID's",
+            "type": "array",
+            "items": {
+                "type": "identifier"
+            }
+        })
+    
+    def remove_channel(arguments):
+        """Remove the specified channel_id 
+        from the set of active channels for bot_id"""
+        try:
+            token = create_token()
+            breadcrumb = create_breadcrumb(token)
+            bot = BotServices.remove_channel(
+                arguments["bot_id"], arguments["channel_id"], 
+                token, breadcrumb)
+            return bot
+        except Exception as e:
+            logger.warning(f"A remove_channel Error has occurred: {e}")
+            return "error"
+    bot.register_action(
+        action_name= "remove_channel", 
+        function = remove_channel,
+        description = "Remove a channel from the active channels list", 
+        arguments_schema = {
+            "description": "Bot and Channel identifier",
+            "type": "object",
+            "properties": {
+                "bot_id": {
+                    "description": "The bot unique identifier",
+                    "type": "identifier"
+                },
+                "channel_id": {
+                    "description": "The channel identifier",
+                    "type": "identifier"
+                }
+            }
+        },
+        output_schema = {
+            "description": "Array (set) of channel ID's",
+            "type": "array",
+            "items": {
+                "type": "identifier"
+            }
+        })
+
     logger.info("Registered Fran command handlers.")
--- src/agents/bot_agent_test.py
+++ src/agents/bot_agent_test.py
@@ -1,12 +1,197 @@
 import unittest
+from unittest.mock import patch, MagicMock
+from echo.agent import Agent
+from agents.bot_agent import create_bot_agent
 
 class TestBotAgent(unittest.TestCase):
-
+    
     def setUp(self):
-        """Re-initialize the config for each test."""
+        """Set up a mock bot agent."""
+        self.mock_bot = Agent("test_bot_agent")
+        create_bot_agent(self.mock_bot)
+        self.get_bot = self.mock_bot.actions["get_bot"]["function"]
+        self.get_channels = self.mock_bot.actions["get_channels"]["function"]
+        self.add_channel = self.mock_bot.actions["add_channel"]["function"]
+        self.remove_channel = self.mock_bot.actions["remove_channel"]["function"]
 
-    def test_something(self):
-        self.assertEqual(True, True)
-
-if __name__ == '__main__':
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.BotServices.get_bot")
+    def test_get_bot_success(self, mock_get_bot, mock_create_token):
+        """Test successful execution of get_bot action."""
+        mock_bot_object = {
+            "_id": "12345",
+            "status": "active",
+            "name": "TestBot",
+            "description": "A test bot",
+            "channels": ["channel_1", "channel_2"],
+            "last_saved": "breadcrumb_data"
+        }
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_get_bot.return_value = mock_bot_object
+        
+        # Call function
+        arguments = "test_bot_id"
+        result = self.get_bot(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_get_bot.assert_called_once_with(arguments, "fake_token")
+        self.assertEqual(result, mock_bot_object)
+    
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.BotServices.get_bot")
+    def test_get_bot_fail(self, mock_get_bot, mock_create_token):
+        """Test failure case for get_bot action."""
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_get_bot.side_effect = Exception("Test Exception")
+        
+        # Call function
+        arguments = {"description": "test_bot_id"}
+        result = self.get_bot(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_get_bot.assert_called_once_with(arguments, "fake_token")
+        self.assertEqual(result, "error")
+        
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.BotServices.get_channels")
+    def test_get_channels_success(self, mock_get_channels, mock_create_token):
+        """Test successful execution of get_bot action."""
+        mock_channels = ["channel_1", "channel_2"]
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_get_channels.return_value = mock_channels
+        
+        # Call function
+        arguments = "test_bot_id"
+        result = self.get_channels(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_get_channels.assert_called_once_with(arguments, "fake_token")
+        self.assertEqual(result, mock_channels)
+    
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.BotServices.get_channels")
+    def test_get_channels_fail(self, mock_get_channels, mock_create_token):
+        """Test failure case for get_bot action."""
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_get_channels.side_effect = Exception("Test Exception")
+        
+        # Call function
+        arguments = {"description": "test_bot_id"}
+        result = self.get_channels(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_get_channels.assert_called_once_with(arguments, "fake_token")
+        self.assertEqual(result, "error")
+        
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.create_breadcrumb")  
+    @patch("agents.bot_agent.BotServices.add_channel")
+    def test_add_channel_success(self, mock_add_channel, mock_create_breadcrumb, mock_create_token):
+        """Test successful execution of get_bot action."""
+        mock_channels = ["channel_1", "channel_2"]
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_add_channel.return_value = mock_channels
+        
+        # Call function
+        arguments = {
+            "bot_id":"test_bot_id",
+            "channel_id":"test_channel"
+        }
+        result = self.add_channel(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_add_channel.assert_called_once_with("test_bot_id", "test_channel", "fake_token", "fake_breadcrumb")
+        self.assertEqual(result, mock_channels)
+    
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.create_breadcrumb")  
+    @patch("agents.bot_agent.BotServices.add_channel")
+    def test_add_channel_fail(self, mock_add_channel, mock_create_breadcrumb, mock_create_token):
+        """Test failure case for get_bot action."""
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_add_channel.side_effect = Exception("Test Exception")
+        
+        # Call function
+        arguments = {
+            "bot_id":"test_bot_id",
+            "channel_id":"test_channel"
+        }
+        result = self.add_channel(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_add_channel.assert_called_once_with(arguments["bot_id"], arguments["channel_id"], "fake_token", "fake_breadcrumb")
+        self.assertEqual(result, "error")
+        
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.create_breadcrumb")  
+    @patch("agents.bot_agent.BotServices.remove_channel")
+    def test_remove_channel_success(self, mock_remove_channel, mock_create_breadcrumb, mock_create_token):
+        """Test successful execution of get_bot action."""
+        mock_channels = ["channel_1", "channel_2"]
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_remove_channel.return_value = mock_channels
+        
+        # Call function
+        arguments = {
+            "bot_id":"test_bot_id",
+            "channel_id":"test_channel"
+        }
+        result = self.remove_channel(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_remove_channel.assert_called_once_with("test_bot_id", "test_channel", "fake_token", "fake_breadcrumb")
+        self.assertEqual(result, mock_channels)
+    
+    @patch("agents.bot_agent.create_token")  
+    @patch("agents.bot_agent.create_breadcrumb")  
+    @patch("agents.bot_agent.BotServices.remove_channel")
+    def test_remove_channel_fail(self, mock_remove_channel, mock_create_breadcrumb, mock_create_token):
+        """Test failure case for get_bot action."""
+        
+        # Mock return values
+        mock_create_token.return_value = "fake_token"
+        mock_create_breadcrumb.return_value = "fake_breadcrumb"
+        mock_remove_channel.side_effect = Exception("Test Exception")
+        
+        # Call function
+        arguments = {
+            "bot_id":"test_bot_id",
+            "channel_id":"test_channel"
+        }
+        result = self.remove_channel(arguments)
+        
+        # Assertions
+        mock_create_token.assert_called_once()
+        mock_create_breadcrumb.assert_called_once_with("fake_token")
+        mock_remove_channel.assert_called_once_with(arguments["bot_id"], arguments["channel_id"], "fake_token", "fake_breadcrumb")
+        self.assertEqual(result, "error")
+        
+if __name__ == "__main__":
     unittest.main()

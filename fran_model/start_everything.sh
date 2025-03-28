79d9a24d5162f891b4d692f539356df3d5ac4bd6
Mike Storey <mike@agile-learning.institute>
2025-03-28 13:56:40 -0400

    Getting prompts organized

--- fran_model/start_everything.sh
+++ fran_model/start_everything.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Initialize all the layers
-./fran_model/initialize_conversation.sh design ./fran_model/prompts/design.csv
 ./fran_model/initialize_conversation.sh echo ./fran_model/prompts/echo.csv
-./fran_model/initialize_conversation.sh exercise ./fran_model/prompts/exercise.csv
-./fran_model/initialize_conversation.sh fran ./fran_model/prompts/fran.csv
 ./fran_model/initialize_conversation.sh tools ./fran_model/prompts/tools.csv
+./fran_model/initialize_conversation.sh design ./fran_model/prompts/design.csv
+./fran_model/initialize_conversation.sh workshop ./fran_model/prompts/workshop.csv
+./fran_model/initialize_conversation.sh exercise ./fran_model/prompts/exercise.csv
 
-# Build a combined prompt
-curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/design" -H "Content-Type: application/json" > /dev/null
-curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/echo" -H "Content-Type: application/json"  > /dev/null
-curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/fran" -H "Content-Type: application/json"  > /dev/null
+# Build FRAN combined prompt
+curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/echo" -H "Content-Type: application/json" > /dev/null
+curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/tools" -H "Content-Type: application/json"  > /dev/null
+curl -s -X POST "http://localhost:8580/api/conversation/FRAN/load/design" -H "Content-Type: application/json"  > /dev/null
